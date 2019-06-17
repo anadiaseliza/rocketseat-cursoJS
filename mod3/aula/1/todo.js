3346dab9b09c05e3d3a8aaf7430353e24e5cfb85
unknown <ana_e_dias@saojose.sc.senai.br>
2019-06-17 08:44:17 -0300

    modulo 3 aula 2

---
+++ mod3/aula/1/todo.js
@@ -0,0 +1,6 @@
+var listElement = document.querySelector("#app ul")
+var listInput = document.querySelector("#app input")
+var listButton = document.querySelector("#app button")
+console.log(listElement);
+console.log(listInput);
+console.log(listButton);
